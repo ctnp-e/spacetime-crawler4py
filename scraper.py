f352da4ecdc4e709a257b1f10efe697badfca0b9
Mary Ivanov <mmivanov@uci.edu>
2026-04-30 17:18:35 -0700

    HOW DID I FORGET THIS

--- scraper.py
+++ scraper.py
@@ -94,6 +94,9 @@ someone said :
 70 subdomains is too little...
 '''
 
+def scraper(url, resp):
+    links = extract_next_links(url, resp)
+    return [link for link in links if is_valid(link)]
 
 def extract_next_links(url, resp):
     ''' Filter by content quality, parse and return links '''
